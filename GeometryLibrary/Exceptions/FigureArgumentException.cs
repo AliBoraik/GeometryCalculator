909c478d8ccd5a73ef81c343f5591ed2ef82d95d
ali <aliaqel2021@gmail.com>
2024-02-09 11:10:18 +0300

    add custom exceptions

---
+++ GeometryLibrary/Exceptions/FigureArgumentException.cs
@@ -0,0 +1,8 @@
+namespace GeometryLibrary.Exceptions;
+
+public class FigureArgumentException : ArgumentException
+{
+    public FigureArgumentException(string message) : base(message)
+    {
+    }
+}
